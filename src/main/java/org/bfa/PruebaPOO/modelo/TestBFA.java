e4acaeac0d95b052dbb1b248449743a54be82744
rjez12 <jesserjrch@uamv.edu.ni>
2026-06-22 21:02:26 -0600

    feat(modelo): añadir entidades TestBFA, RespuestaCandidato, Resultado y BaremoNormativo
    Mapea los componentes principales del módulo de evaluación, incluyendo el control de estados de los tests, almacenamiento de respuestas y estructuras para el cálculo de percentiles métricos.

---
+++ src/main/java/org/bfa/PruebaPOO/modelo/TestBFA.java
@@ -0,0 +1,39 @@
+package org.bfa.PruebaPOO.modelo;
+import javax.persistence.*;
+
+import lombok.*;
+import org.openxava.annotations.DescriptionsList;
+import org.openxava.annotations.Hidden;
+import org.openxava.annotations.Required;
+import java.time.LocalDateTime;
+import java.util.Collection;
+
+@Entity
+@Getter @Setter
+public class TestBFA {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Hidden
+    private Integer idtest;
+
+    @Required
+    private LocalDateTime fechacreacion;
+
+    @Required
+    private Integer tiempolimitesegundos = 900; // 15 minutos estrictos
+
+    private Integer segundosrestantes;
+
+    @Enumerated(EnumType.STRING)
+    @Required
+    private EnumestadoTest estado;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @DescriptionsList // openxava dibujará un combo box con los candidatos
+    private Candidato candidato;
+
+    @OneToMany(mappedBy = "testbfa", cascade = CascadeType.ALL)
+    private Collection<RespuestaCandidato> respuestas;
+
+}
